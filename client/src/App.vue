37434ac52e63ed713926a30ec879d4f782c8f6ec
Jinhyuk Park <jh.park@hdjunction.co.kr>
2019-08-30 10:06:59 +0900

    Init hisc client project

---
+++ client/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-layout text-center wrap>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </v-layout>
+    </v-container>
+    <router-view />
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data: () => ({
+      //
+    }),
+  }
+</script>
